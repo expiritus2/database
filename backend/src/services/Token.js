d2309869857df99f3c509351d0f54766cf3bda87
Mikhail <michaildrazdovich@gmail.com>
2021-07-14 23:02:03 +0300

    Add expiresIn to jwt

--- backend/src/services/Token.js
+++ backend/src/services/Token.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 class Token {
-    static sign(params, salt = process.env.TOKEN_SALT, options = {}) {
+    static sign(params, salt = process.env.TOKEN_SALT, options = { expiresIn: '1 days'}) {
         return jwt.sign(params, salt, options);
     }
 
